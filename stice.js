4294dccc326b4c175b12efd140ddc1e7c01e3a62
Satria Salim <77188249+SalimQS@users.noreply.github.com>
2024-06-19 09:34:36 +0800

    Update stice.js

--- stice.js
+++ stice.js
@@ -1,25 +1,23 @@
 function encryptToInteger(str) {
-    let encrypted = '';
-    console.log('Encrypt:')
-    for (let i = 0; i < str.length; i++) {
-        let charCode = str.charCodeAt(i).toString();
-        console.log('\n'+str[i]+': '+charCode)
-        while (charCode.length < 3) {
-            charCode = '0' + charCode; 
-        }
-        encrypted += charCode;
+  let encrypted = "";
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i).toString();
+    while (charCode.length < 3) {
+      charCode = "0" + charCode;
     }
-    return encrypted;
+    encrypted += charCode;
+  }
+  return encrypted;
 }
 
 function decryptFromInteger(encryptedStr) {
-    let decrypted = '';
-    for (let i = 0; i < encryptedStr.length; i += 3) {
-        let charCode = encryptedStr.substr(i, 3);
-        if (charCode[0] === '0') {
-            charCode = charCode.replace(/^0+/, '');
-        }
-        decrypted += String.fromCharCode(parseInt(encryptedStr.substr(i, 3), 10))
+  let decrypted = "";
+  for (let i = 0; i < encryptedStr.length; i += 3) {
+    let charCode = encryptedStr.substr(i, 3);
+    if (charCode[0] === "0") {
+      charCode = charCode.replace(/^0+/, "");
     }
-    return decrypted
+    decrypted += String.fromCharCode(parseInt(encryptedStr.substr(i, 3), 10));
+  }
+  return decrypted;
 }
