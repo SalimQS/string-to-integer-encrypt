c97d8a3dfe6da5e81a830a9ba9dacb9e8d836af2
Satria Salim <77188249+SalimQS@users.noreply.github.com>
2024-06-19 09:33:13 +0800

    Update stice.js

--- stice.js
+++ stice.js
@@ -1,23 +1,25 @@
-function IntegerEncrypt(str) {
-  let encrypted = "";
-  for (let i = 0; i < str.length; i++) {
-    let charCode = str.charCodeAt(i).toString();
-    while (charCode.length < 3) {
-      charCode = "0" + charCode;
+function encryptToInteger(str) {
+    let encrypted = '';
+    console.log('Encrypt:')
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i).toString();
+        console.log('\n'+str[i]+': '+charCode)
+        while (charCode.length < 3) {
+            charCode = '0' + charCode; 
+        }
+        encrypted += charCode;
     }
-    encrypted += charCode;
-  }
-  return encrypted;
+    return encrypted;
 }
 
-function IntegerDecrypt(encryptedStr) {
-  let decrypted = "";
-  for (let i = 0; i < encryptedStr.length; i += 3) {
-    let charCode = encryptedStr.substr(i, 3);
-    if (charCode[0] === "0") {
-      charCode = charCode.replace(/^0+/, "");
+function decryptFromInteger(encryptedStr) {
+    let decrypted = '';
+    for (let i = 0; i < encryptedStr.length; i += 3) {
+        let charCode = encryptedStr.substr(i, 3);
+        if (charCode[0] === '0') {
+            charCode = charCode.replace(/^0+/, '');
+        }
+        decrypted += String.fromCharCode(parseInt(encryptedStr.substr(i, 3), 10))
     }
-    decrypted += String.fromCharCode(parseInt(encryptedStr.substr(i, 3), 10));
-  }
-  return decrypted;
+    return decrypted
 }
